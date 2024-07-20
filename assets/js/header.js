21c68f2b16842992efe3073a7f0f6204b600d841
taydp2002 <taydp2002@gmail.com>
2024-07-20 15:40:41 +0700

    update theme

--- assets/js/header.js
+++ assets/js/header.js
@@ -8,3 +8,9 @@ mobileMenuOpen.addEventListener('click',()=>{
 mobileMenuClose.addEventListener('click',()=>{
     headerBody.classList.remove('headerMobile');
 })
+function categoryBlog() {
+    headerBody.classList.toggle('categoryBlog');
+}
+function categoryProduct() {
+    headerBody.classList.toggle('categoryProduct');
+}
